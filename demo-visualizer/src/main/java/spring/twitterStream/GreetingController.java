80ee433a02842e25eb1edf4de46d4566a3f822c4
nikste <nikolaas.steenbergen@gmail.com>
2015-11-04 12:17:20 +0100

    receives twitter data now

---
+++ demo-visualizer/src/main/java/spring/twitterStream/GreetingController.java
@@ -0,0 +1,23 @@
+package spring.twitterStream;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    public static List<String> dataBuffer = new ArrayList<String>();
+
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return dataBuffer.toString();//new Greeting(0, dataBuffer.toString());
+    }
+}
